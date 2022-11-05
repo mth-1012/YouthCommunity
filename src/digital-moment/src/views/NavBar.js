c17f85d41b6a877a8b6826c9c953eb5ce90eb496
yida-li <gabrielyidali@gmail.com>
2022-11-05 19:26:50 -0400

    a search button

--- src/digital-moment/src/views/NavBar.js
+++ src/digital-moment/src/views/NavBar.js
@@ -14,7 +14,10 @@ import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import axios from "axios";
 import AddPost from "./AddPost";
+import { styled, alpha } from '@mui/material/styles';
 import {useNavigate} from "react-router-dom";
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
 import {
   Dialog,
   DialogActions,
@@ -46,7 +49,47 @@ function NavBar() {
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
-
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
     navigate("/homelocal");
@@ -137,6 +180,24 @@ function NavBar() {
               </Button>
             ))}
           </Box>
+
+
+      
+
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+
+
+
+
+
 
           <Box sx={{mr: 2}}>
             <Button sx={{color: "white"}} onClick={() => setOpen(true)}>
